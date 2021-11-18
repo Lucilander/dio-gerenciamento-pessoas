fc2598991cbe8c4ecc009ce0f4bd1e813048c7a8
Lucilander <lucilander1@gmail.com>
2021-11-18 15:44:32 -0300

    Impoved Person Creation Funcionality

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,6 +1,7 @@
 package one.digitalinnovation.personapi.service;
 
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +12,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class PersonService {
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
-    public MessageResponseDTO createPerson( Person person) {
-        Person savedPerson = personRepository.save(person);
+
+    public MessageResponseDTO createPerson( PersonDTO personDTO) {
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personDTO);
         return MessageResponseDTO.builder().message("Created person with ID: " + savedPerson.getId()).build();
     }
 }
